01b181c84ebe7d86d9cbd8dffc86bb5ad9ccaf47
haya573 <nakasai.hayato@gmail.com>
2025-01-01 21:57:27 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// http.ResponseWriter: クライアントにデータを返すためのオブジェクト。
+// *http.Request: クライアントからのリクエスト情報。
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+// http.ListenAndServe(":8080", nil): ポート 8080 でサーバーを起動します。nil はデフォルトのマルチプレクサを使用することを意味します。
+// fmt.Fprintf(w, "Hello, World!"): クライアントに Hello, World! をレスポンスとして送信します。
+func main() {
+	// http.HandleFunc("/", homeHandler)     // ホームページ
+	http.HandleFunc("/about", aboutHandler) // Aboutページ
+	http.HandleFunc("/hello", helloHandler) // "/hello" のハンドラ
+	// 静的ファイルを提供
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	fmt.Println("サーバーをポート8080で起動中...")
+	http.ListenAndServe(":8080", nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Home Page!")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the About Page.")
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name") // クエリパラメータ "name" を取得
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello, %s!", name)
+}
